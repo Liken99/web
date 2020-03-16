cbfb88ab792be7c5fd209e9e7aac2f03fd4cec18
Liken99 <47985395+Liken99@users.noreply.github.com>
2020-03-16 17:08:56 +0300

    Создали Framework

---
+++ views/site/about.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1>О Компании</h1>
+
+    <p>
+        Текст о компании
+    </p>
+</div>
