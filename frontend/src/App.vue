55134d17968b172ce96de2dd4e1417c636374e07
liken99 <MrForever99>
2020-04-27 17:07:30 +0300

    Работа с VUE.js Разработка сайта на VUE

---
+++ frontend/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Carousel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">О нас</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/films">Фильмы</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <main>
+      <div class="container">
+        <router-view/>
+      </div>
+    </main>
+
+    <footer>
+
+    </footer>
+  </div>
+</template>
+
+<style>
+  main > .container {
+    padding-top: 65px;
+  }
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Главная страница</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
